52ece1e90b7a3cda45888be8d2d9edaf702f0daf
wo9374 <wo9374@naver.com>
2022-04-04 12:05:03 +0900

    Base 적용대기

---
+++ app/src/main/java/com/example/instagramclone/base/BaseFragment.kt
@@ -0,0 +1,39 @@
+package com.example.instagramclone.base
+
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.Fragment
+
+abstract class BaseFragment<T : ViewDataBinding>(private val layoutId: Int) : Fragment() {
+    private var _binding: T? = null
+    val binding:T get() = _binding!!
+
+    lateinit var mContext: Context
+    //lateinit var navController: NavController
+
+    //return layout resource id
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        mContext = context
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        _binding = DataBindingUtil.inflate(inflater, layoutId, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        //navController = Navigation.findNavController(binding.root)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding.unbind()
+    }
+}
