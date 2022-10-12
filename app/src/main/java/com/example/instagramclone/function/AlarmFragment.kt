25e8d301d203b3cb17479e408ed7c3e3c1660ab4
jblee9374 <55440637+wo9374@users.noreply.github.com>
2022-10-12 17:19:10 +0900

    Fuction Fragment 추가

--- app/src/main/java/com/example/instagramclone/function/AlarmFragment.kt
+++ app/src/main/java/com/example/instagramclone/function/AlarmFragment.kt
@@ -6,7 +6,7 @@ import com.example.instagramclone.R
 import com.example.instagramclone.base.BaseFragment
 import com.example.instagramclone.databinding.FragmentDetailViewBinding
 
-class DetailViewFragment : BaseFragment<FragmentDetailViewBinding>(R.layout.fragment_detail_view) {
+class AlarmFragment : BaseFragment<FragmentDetailViewBinding>(R.layout.fragment_detail_view) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
--- app/src/main/java/com/example/instagramclone/function/GridFragment.kt
+++ app/src/main/java/com/example/instagramclone/function/GridFragment.kt
@@ -6,7 +6,7 @@ import com.example.instagramclone.R
 import com.example.instagramclone.base.BaseFragment
 import com.example.instagramclone.databinding.FragmentDetailViewBinding
 
-class DetailViewFragment : BaseFragment<FragmentDetailViewBinding>(R.layout.fragment_detail_view) {
+class GridFragment : BaseFragment<FragmentDetailViewBinding>(R.layout.fragment_grid) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
