91fb3bdd4a65f4a28abc5467becb218f5b289816
jblee9374 <55440637+wo9374@users.noreply.github.com>
2022-10-13 09:02:21 +0900

    Create HomeFragment.kt

---
+++ app/src/main/java/com/example/instagramclone/function/HomeFragment.kt
@@ -0,0 +1,14 @@
+package com.example.instagramclone.function
+
+import android.os.Bundle
+import android.view.View
+import com.example.instagramclone.R
+import com.example.instagramclone.base.BaseFragment
+import com.example.instagramclone.databinding.FragmentDetailViewBinding
+
+class HomeFragment : BaseFragment<FragmentDetailViewBinding>(R.layout.fragment_home) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
