6354cfc08a074e01d527c62bf46747bfa8b92df5
jblee9374 <55440637+wo9374@users.noreply.github.com>
2022-10-13 09:03:03 +0900

    Navigation BottomNavi 연결

--- app/src/main/java/com/example/instagramclone/function/MainActivity.kt
+++ app/src/main/java/com/example/instagramclone/function/MainActivity.kt
@@ -2,11 +2,24 @@ package com.example.instagramclone.function
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.example.instagramclone.R
+import com.example.instagramclone.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.lifecycleOwner = this
+
+        NavigationUI.setupWithNavController(
+            binding.bottomNavigation,
+            findNavController(R.id.main_content)
+        )
     }
 }
